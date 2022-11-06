92d8dadfcd6c7e75120e913f81603a500c36d71e
Eduardo <earosselot@gmail.com>
2022-11-06 17:42:33 -0300

    express hello world

---
+++ 02-express-hello-world/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = 8080
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Hello world!')
+})
+
+app.listen(port, () => {
+	console.log(`Express app listening on port ${port}`)
+})
